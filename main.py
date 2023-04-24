252ac83f261e659c6fb83e2cb5458e1d10c78a1e
rishabhAditya <131591491+rishabhAditya@users.noreply.github.com>
2023-04-24 12:36:11 +0000

    added json data and bootstrap

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
+
+jobs= [
+  {
+    
+    "id1":1,
+    "title":"Data Analyst",
+    "location":"Delhi",
+    "salary":500000
+  
+  },
+  {
+    
+    "id1":2,
+    "title":"Web developer",
+    "location":"Delhi"
+  
+  },
+   {
+    
+    "id1":3,
+    "title":"Cyber Engineer",
+    "location":"WFH",
+    "salary":7600000
+  
+  }
+]
 
 @app.route('/')
 def start():
-  return render_template('home.html')
+  
+  return render_template('index.html',j=jobs)
+@app.route('/api/jobs')
+def api():
+  return jsonify(jobs)
 app.run(host='0.0.0.0',debug = True)
